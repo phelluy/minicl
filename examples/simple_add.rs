d85830e40a49dea26a676ee4a3611817e75c51a5
helluy <helluy@unistra.fr>
2022-01-16 16:37:46 +0100

    up

--- examples/simple_add.rs
+++ examples/simple_add.rs
@@ -11,13 +11,15 @@ fn main() {
 
     let mut cldev = minicl::Accel::new(source);
 
-    let n = 64;
-
-    let v : Vec<i32> = vec![12; n];
-
+    // the used kernels has to be registered
     let kname = "simple_add".to_string();
     cldev.register_kernel(&kname);
 
+    let n = 64;
+
+    // the memory buffer shared with the 
+    // accelerator has to be registered
+    let v : Vec<i32> = vec![12; n];
     let v = cldev.register_buffer(v);
 
     let x: i32 = 1000;
@@ -25,13 +27,20 @@ fn main() {
     let globsize = n;
     let locsize= 16;
 
+    // invoke this macro for the first kernel call
     kernel_set_args_and_run!(cldev, kname, globsize, locsize, v, x);
 
+    // map the buffer for access from the host 
     let v: Vec<i32> = cldev.map_buffer(v);
     println!("First kernel run v={:?}",v);
 
+    // unmap for giving it back to the device
     let v = cldev.unmap_buffer(v);
+
+    // next call: no need to redefine the kernel args
+    // if they are the same
     cldev.run_kernel(&kname, globsize, locsize);
+
     let v: Vec<i32> = cldev.map_buffer(v);
     println!("Next kernel run v={:?}",v);
 
